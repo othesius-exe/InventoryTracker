9a6df928157e73b6f1175d6bb006a2598633d626
othesius-exe <othesius.exe@gmail.com>
2017-05-27 11:12:20 -0400

    First Commit

---
+++ app/src/main/java/com/example/android/inventorytracker/InventoryCatalog.java
@@ -0,0 +1,13 @@
+package com.example.android.inventorytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryCatalog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_catalog);
+    }
+}

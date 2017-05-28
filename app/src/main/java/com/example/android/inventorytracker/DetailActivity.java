bd0979bc5cb549301ddeb1e7f837494fd1a25904
othesius-exe <othesius.exe@gmail.com>
2017-05-28 15:04:07 -0400

    added some views to details page

--- app/src/main/java/com/example/android/inventorytracker/DetailActivity.java
+++ app/src/main/java/com/example/android/inventorytracker/DetailActivity.java
@@ -1,8 +1,25 @@
 package com.example.android.inventorytracker;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
 /**
  * Activity to handle displaying details of individual items in the inventory.
  */
 
-public class DetailActivity {
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate an options menu from the res menu_detail.xml file
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
 }
--- app/src/main/java/com/example/android/inventorytracker/InventoryCatalog.java
+++ app/src/main/java/com/example/android/inventorytracker/InventoryCatalog.java
@@ -1,7 +1,11 @@
 package com.example.android.inventorytracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
 
 public class InventoryCatalog extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class InventoryCatalog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_catalog);
+
+        // Setup the FAB to open the editor Activity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(InventoryCatalog.this, DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from the res menu_catalog.xml file
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
     }
 }
